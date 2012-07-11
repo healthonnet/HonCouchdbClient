ac24dcb8404049c8d073c89555317c5207db893d
Nolan Lawson <nolan.lawson@gmail.com>
2012-07-11 17:08:04 +0200

    update CouchDB urls

--- src/main/java/org/healthonnet/couchdbclient/Constants.java
+++ src/main/java/org/healthonnet/couchdbclient/Constants.java
@@ -2,7 +2,7 @@ package org.healthonnet.couchdbclient;
 
 public class Constants {
 
-	public static final String HOST = "ec2-176-34-93-34.eu-west-1.compute.amazonaws.com";
+	public static final String HOST = "ec2-46-51-139-19.eu-west-1.compute.amazonaws.com";
 	public static final int PORT = 5984;
 	public static final String DATABASE = "khresmoi_docs";
 	
