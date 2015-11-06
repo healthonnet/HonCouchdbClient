be996dc739ef5469dd0b82901c9852b61be1c7df
William Belle <william.belle@gmail.com>
2015-11-06 12:34:23 +0100

    update couchdb url

--- src/main/java/org/healthonnet/couchdbclient/Constants.java
+++ src/main/java/org/healthonnet/couchdbclient/Constants.java
@@ -2,8 +2,8 @@ package org.healthonnet.couchdbclient;
 
 public class Constants {
 
-	public static final String HOST = "khresmoi.honservices.org";
+	public static final String HOST = "couchdb.khresmoi.eu";
 	public static final int PORT = 5984;
 	public static final String DATABASE = "khresmoi_docs";
-	
+
 }
